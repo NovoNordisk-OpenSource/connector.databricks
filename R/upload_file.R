cada2e58b652c56c9b325dfedfdafe3a9c265960
SFFL (Steffen Falgreen Larsen) <sffl@novonordisk.com>
2024-06-03 08:44:44 +0200

    chore: remove redundant helper functions

--- R/upload_file.R
+++ R/upload_file.R
@@ -3,16 +3,35 @@
 #' modify the bytes before sending. The contents of the resulting file will be
 #' exactly the bytes sent in the request body. If the request is successful,
 #' there is no response body.
-#' @param client Required. Instance of DatabricksClient()
 #'
+#' @param client Required. Instance of DatabricksClient()
 #' @param contents This field has no description yet.
 #' @param file_path Required. The absolute path of the file.
 #' @param overwrite If true, an existing file will be overwritten.
 #'
 #'
 #' @rdname upload_file
-#' @alias filesUpload
+#'
+#' @examples
+#' # In order to connect to databricks on environments where configurations are
+#' # available via the environment variable DATABRICKS_CONFIG_FILE or located
+#' # at ~/.databrickscfg - simply write
+#' db_client <- DatabricksClient()
+#'
+#' # To check if connection is established
+#' open_connection <- db_client$debug_string() != ""
+#'
+#' if (open_connection) {
+#'   tempfile_orig <- tempfile(fileext = ".txt")
+#'   writeLines(letters, tempfile_orig)
+#'
+#'   databrics_volume <- "/Volumes/my_adam/tester"
+#'   databricks_file <- file.path(databrics_volume, basename(tempfile_orig))
+#'
+#'   upload_file(db_client, databricks_file, contents = tempfile_orig)
+#' }
 #' @export
+#' @importFrom httr upload_file
 upload_file <- function(client, file_path, contents, overwrite = TRUE) {
 
   if (is.character(contents))
@@ -24,8 +43,3 @@ upload_file <- function(client, file_path, contents, overwrite = TRUE) {
             body = contents,
             query = query, json_wrap_body = FALSE)
 }
-
-#' @rdname upload_file
-#' @export
-filesUpload <- upload_file
-
