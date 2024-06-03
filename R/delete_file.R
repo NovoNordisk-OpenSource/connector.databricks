a04bbe2f743f010222675b7d7e855df9fc601395
SFFL (Steffen Falgreen Larsen) <sffl@novonordisk.com>
2024-06-03 23:03:04 +0200

    feat: create functionality for deleting a file

---
+++ R/delete_file.R
@@ -0,0 +1,15 @@
+#' Delete a file.
+#'
+#' Deletes a file. If the request is successful, there is no response body.
+#' @param client Required. Instance of DatabricksClient()
+#'
+#' @param file_path Required. The absolute path of the file.
+#'
+#'
+#' @rdname delete_file
+#' @alias filesDelete
+#' @export
+delete_file <- function(client, file_path) {
+
+  client$do("DELETE", paste("/api/2.0/fs/files", file_path, sep = ""))
+}
