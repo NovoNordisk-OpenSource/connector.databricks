ce61cbad6a27966f90fc843b245e056619dee1dc
SFFL (Steffen Falgreen Larsen) <sffl@novonordisk.com>
2024-06-03 23:03:49 +0200

    feat: add function for removing a firectory

---
+++ R/delete_file_directory.R
@@ -0,0 +1,19 @@
+#' Delete a directory.
+#'
+#' Deletes an empty directory.
+#'
+#' To delete a non-empty directory, first delete all of its contents. This can
+#' be done by listing the directory contents and deleting each file and
+#' subdirectory recursively.
+#' @param client Required. Instance of DatabricksClient()
+#'
+#' @param directory_path Required. The absolute path of a directory.
+#'
+#'
+#' @rdname delete_file_directory
+#' @alias filesDeleteDirectory
+#' @export
+delete_file_directory <- function(client, directory_path) {
+
+  client$do("DELETE", paste("/api/2.0/fs/directories", directory_path, sep = ""))
+}
