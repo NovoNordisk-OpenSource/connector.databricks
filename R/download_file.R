11a22f3c05cfc25285512ebcbf719adb2c91552d
SFFL (Steffen Falgreen Larsen) <sffl@novonordisk.com>
2024-06-03 08:44:44 +0200

    chore: remove redundant helper functions

--- R/download_file.R
+++ R/download_file.R
@@ -1,12 +1,14 @@
 #' Downloads a file of up to 5 GiB. The file contents are the response body.
 #' This is a standard HTTP file download, not a JSON RPC.
+#'
 #' @param client Required. Instance of DatabricksClient()
-#'
 #' @param file_path Required. The absolute path of the file.
-#'
+#' @param local_path local path for the file.
+#' if \code{NULL} the response from databricks is returned.
 #'
 #' @rdname download_file
-#' @alias filesDownload
+#' @returns The repsonse from databricks.
+#' If \code{!is.null(local_path)} the response is returned invisible.
 #' @export
 download_file <- function(client, file_path, local_path = NULL) {
 
@@ -14,11 +16,8 @@ download_file <- function(client, file_path, local_path = NULL) {
 
   if (!is.null(local_path)) {
     writeBin(response, con = local_path)
+    return(invisible(response))
   } else {
     return(response)
   }
 }
-
-#' @rdname download_file
-#' @export
-filesDownload <- download_file
