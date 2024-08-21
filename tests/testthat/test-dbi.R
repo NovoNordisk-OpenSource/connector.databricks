9895538ccb3844d366056d246de816a37b052e8f
LVGK (Lovemore Gakava) <lvgk@novonordisk.com>
2024-08-21 10:36:24 +0200

    test-dbi and test-dbi_methods are testing the same functionalities. Intergrating them into one file.

--- tests/testthat/test-dbi.R
+++ tests/testthat/test-dbi.R
@@ -1,54 +1,84 @@
-test_that("connector_databricks_dbi", {
+# Create a temporary dataset for testing
+create_temp_dataset <- function() {
+  # Sample data
+  x <- mtcars
+  x$car <- rownames(x)
+  rownames(x) <- NULL
+  return(x)
+}
 
-  skip_on_ci()
-  skip_on_cran()
+test_that(paste("DBI generics work for connector_databricks_dbi"), {
 
-  # TODO: Is there a way to mask this information, or make it available in the CI?
+  if (!is.null(Sys.getenv("http_path_local")) &
+      !is.null(Sys.getenv("catalog_local")) &
+      !is.null(Sys.getenv("schema_local"))) {
 
-  con_databricks <- connector_databricks_dbi$new(
-    http_path = "sql/protocolv1/o/273240340063409/0216-121054-v8tdvp00",
-    catalog = "amace_cdr_bronze_dev",
-    schema = "my_adam"
-  ) |>
-    expect_no_condition()
+    skip_on_ci()
+    skip_on_cran()
 
-  # Note: Below is almost the same code as in the example to make sure it runs
+    # Retrieve the stored values
+    http_path_local <- Sys.getenv("http_path_local")
+    catalog_local <- Sys.getenv("catalog_local")
+    schema_local <- Sys.getenv("schema_local")
 
-  # List tables in my_schema
+    temp_table_name <- paste0("temp_mtcars_", format(Sys.time(), "%Y%m%d%H%M%S"))
 
-  con_databricks$list_content() |>
-    expect_type("character")
+    expect_error(connector_databricks_dbi$new(http_path = 1))
 
-  # Read and write tables
+    # initialized with appropriate values for catalog, schema, and conn
+    cnt <- connector_databricks_dbi$new(
+      http_path = http_path_local,
+      catalog = catalog_local,
+      schema = schema_local)
 
-  cars <- mtcars
-  cars$name <- rownames(cars)
-  rownames(cars) <- NULL
+    cnt |>
+      expect_no_error()
 
-  con_databricks$write(cars, "my_mtcars_table", overwrite = TRUE) |>
-    expect_no_condition()
+    checkmate::assert_r6(
+      cnt,
+      classes = c("connector_databricks_dbi"),
+      private = c(".catalog", ".schema")
+    )
 
-  con_databricks$read("my_mtcars_table") |>
-    expect_s3_class("data.frame") |>
-    expect_equal(cars)
+    cnt$cnt_write(create_temp_dataset(), temp_table_name) |>
+      expect_no_condition()
 
-  # Use dplyr::tbl
+    cnt$cnt_list_content() |>
+      expect_contains(temp_table_name)
 
-  cars_tbl <- con_databricks$tbl("my_mtcars_table")
-  expect_true(dplyr::is.tbl(cars_tbl))
-  cars_tbl |>
-    dplyr::collect() |>
-    expect_no_condition() |>
-    as.data.frame() |>
-    expect_equal(cars)
+    cnt$cnt_write(create_temp_dataset(), temp_table_name) |>
+      expect_error()
 
-  # Remove table
+    cnt$cnt_read(temp_table_name) |>
+      expect_equal(create_temp_dataset())
 
-  con_databricks$remove("my_mtcars_table") |>
-    expect_no_condition()
+    cnt$cnt_write(create_temp_dataset(), temp_table_name, overwrite = TRUE) |>
+      expect_no_condition()
 
-  # Disconnect
+    cnt$cnt_tbl(temp_table_name) |>
+      dplyr::filter(car == "Mazda RX4") |>
+      dplyr::select(car, mpg) |>
+      dplyr::collect() |>
+      expect_equal(dplyr::tibble(car = "Mazda RX4", mpg = 21))
 
-  con_databricks$disconnect() |>
-    expect_no_condition()
+    # cnt$conn |>
+    #   DBI::dbGetQuery("SELECT * FROM temp_table_name") |>
+    #   expect_equal(create_temp_dataset())
+
+    cnt$cnt_remove(temp_table_name) |>
+      expect_no_condition()
+
+    cnt$cnt_disconnect() |>
+      expect_no_condition()
+
+    tryCatch(
+      cnt$cnt_read(temp_table_name),
+      error = function(e) {
+        return("Error occurred")
+      }
+    ) |>
+      expect_equal("Error occurred")
+  } else {
+    skip("Skipping test as http_path_local, catalog_local, or schema_local is NULL")
+  }
 })
