0142a234b77f4457b3bc5a6d9fa0dfbe59beb4aa
Vladimir Obucina <vlob@novonordisk.com>
2024-12-09 16:28:39 +0100

    Fix: CI fails with skip_on_ci

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,6 +1,4 @@
 ### Script for setting up tests
-skip_on_ci()
-
 testing_env_variables <- c(
   "DATABRICKS_VOLUME",
   "DATABRICKS_CATALOG_NAME",
@@ -8,13 +6,11 @@ testing_env_variables <- c(
 )
 
 if (!all(testing_env_variables %in% names(Sys.getenv()))) {
+  skip_on_ci()
   cli::cli_abort("Not all testing parameters are set. Please set environment variables:
     DATABRICKS_VOLUME, DATABRICKS_CATALOG_NAME and DATABRICKS_SCHEMA_NAME in order to be
   able to test the package.")
 }
-
-# Dummy DatabricksClient object (used for failing tests)
-dummy_db_client <- list(a = 1, b = 2)
 
 # Databricks catalog used throughout tests
 setup_db_catalog <- Sys.getenv("DATABRICKS_CATALOG_NAME")
