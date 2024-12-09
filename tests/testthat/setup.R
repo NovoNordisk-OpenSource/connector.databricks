152dd43467ddb5026870c4a2f8a3427762a2eb4a
Vladimir Obucina <vlob@novonordisk.com>
2024-12-09 17:02:16 +0100

    Fix CI issues with skipping authentication.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -5,40 +5,41 @@ testing_env_variables <- c(
   "DATABRICKS_SCHEMA_NAME"
 )
 
-if (!all(testing_env_variables %in% names(Sys.getenv()))) {
-  skip_on_ci()
-  cli::cli_abort("Not all testing parameters are set. Please set environment variables:
-    DATABRICKS_VOLUME, DATABRICKS_CATALOG_NAME and DATABRICKS_SCHEMA_NAME in order to be
-  able to test the package.")
-} else {
-  # Databricks catalog used throughout tests
-  setup_db_catalog <- Sys.getenv("DATABRICKS_CATALOG_NAME")
-
-  # Databricks schema used throughout tests
-  setup_db_schema <- Sys.getenv("DATABRICKS_SCHEMA_NAME")
-
-  # Setup databricks volume path
-  setup_databricks_volume <- Sys.getenv("DATABRICKS_VOLUME")
-
-  # Databricks volume used throughout tests
-  setup_db_volume_path <- paste(setup_db_catalog, setup_db_schema, "local_test_volume", sep = "/")
-
-  # Connector object testing
-  setup_volume_connector <- connector_databricks_volume(
-    catalog = setup_db_catalog,
-    schema = setup_db_schema,
-    path = "local_test_volume",
-    force = TRUE
-  )
-
-  ##  Run after all tests
-  # Placeholder for whatever needs to be removed in the end
-  withr::defer(
-    delete_databricks_volume(
-      name = "local_test_volume",
-      catalog_name = setup_db_catalog,
-      schema_name = setup_db_schema
-    ),
-    teardown_env()
-  )
+if (isFALSE(as.logical(Sys.getenv("CI", "false")))) {
+  if (!all(testing_env_variables %in% names(Sys.getenv()))) {
+    cli::cli_abort("Not all testing parameters are set. Please set environment variables:
+      DATABRICKS_VOLUME, DATABRICKS_CATALOG_NAME and DATABRICKS_SCHEMA_NAME in order to be
+    able to test the package.")
+  } else {
+    # Databricks catalog used throughout tests
+    setup_db_catalog <- Sys.getenv("DATABRICKS_CATALOG_NAME")
+  
+    # Databricks schema used throughout tests
+    setup_db_schema <- Sys.getenv("DATABRICKS_SCHEMA_NAME")
+  
+    # Setup databricks volume path
+    setup_databricks_volume <- Sys.getenv("DATABRICKS_VOLUME")
+  
+    # Databricks volume used throughout tests
+    setup_db_volume_path <- paste(setup_db_catalog, setup_db_schema, "local_test_volume", sep = "/")
+  
+    # Connector object testing
+    setup_volume_connector <- connector_databricks_volume(
+      catalog = setup_db_catalog,
+      schema = setup_db_schema,
+      path = "local_test_volume",
+      force = TRUE
+    )
+  
+    ##  Run after all tests
+    # Placeholder for whatever needs to be removed in the end
+    withr::defer(
+      delete_databricks_volume(
+        name = "local_test_volume",
+        catalog_name = setup_db_catalog,
+        schema_name = setup_db_schema
+      ),
+      teardown_env()
+    )
+  }  
 }
