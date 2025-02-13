4694c2fd957498bf51a3a14f1b73b01913152b50
CGID (cervan girard) <cgid@novonordisk.com>
2025-02-13 07:34:14 +0100

    fix: skip Tests on CI

--- tests/testthat/test-cnt_logger_databricks.R
+++ tests/testthat/test-cnt_logger_databricks.R
@@ -20,6 +20,8 @@ test_that(
   "log_read_connector.connector_databricks_dbi logs correct message",
   {
     skip_if_not_installed("whirl")
+    skip_on_cran()
+    skip_on_ci()
 
     # Create mock for whirl::log_read
     log_mock <- mockery::mock()
@@ -52,6 +54,8 @@ test_that(
   "log_write_connector.connector_databricks_dbi logs correct message",
   {
     skip_if_not_installed("whirl")
+    skip_on_cran()
+    skip_on_ci()
 
     # Create mock for whirl::log_write
     log_mock <- mockery::mock()
@@ -85,6 +89,9 @@ test_that(
   "log_remove_connector.connector_databricks_dbi logs correct message",
   {
     skip_if_not_installed("whirl")
+    skip_on_cran()
+    skip_on_ci()
+
     # Create mock for whirl::log_delete
     log_mock <- mockery::mock()
     mockery::stub(
@@ -115,89 +122,87 @@ test_that(
 
 
 # Define the environment variable for the full path
+if(   isFALSE(as.logical(Sys.getenv("CI", "false")))){
+  get_full_path <- paste0("/Volumes/",setup_db_volume_path)
+  if (get_full_path != "") {
+    # Skip tests if the connector.databricks package is not installed
+    skip_if_not_installed("whirl")
 
-get_full_path <- paste0("/Volumes/",setup_db_volume_path)
-if (get_full_path != "") {
-  # Skip tests if the connector.databricks package is not installed
-  skip_if_not_installed("whirl")
+    # Create a mock ConnectorDatabricksVolume object with a temporary folder path
+    db_vol_connector <- connector.databricks::ConnectorDatabricksVolume$new(
+      full_path = setup_db_volume_path,
+      extra_class = "connector_logger",
+      force = TRUE
+    )
 
-  # Create a mock ConnectorDatabricksVolume object with a temporary folder path
-  db_vol_connector <- connector.databricks::ConnectorDatabricksVolume$new(
-    full_path = setup_db_volume_path,
-    extra_class = "connector_logger",
-    force = TRUE
-  )
+    test_that(
+      "log_read_connector.ConnectorDatabricksVolume logs correct message",
+      {
+        # Create mock for whirl::log_read
+        log_mock <- mockery::mock()
+        mockery::stub(
+          log_read_connector.ConnectorDatabricksVolume,
+          "whirl::log_read",
+          log_mock
+        )
 
-  test_that(
-    "log_read_connector.ConnectorDatabricksVolume logs correct message",
-    {
-      # Create mock for whirl::log_read
-      log_mock <- mockery::mock()
-      mockery::stub(
-        log_read_connector.ConnectorDatabricksVolume,
-        "whirl::log_read",
-        log_mock
-      )
+        # Test the function
+        log_read_connector.ConnectorDatabricksVolume(db_vol_connector, "test.csv")
 
-      # Test the function
-      log_read_connector.ConnectorDatabricksVolume(db_vol_connector, "test.csv")
+        # Verify log_read was called with correct message
+        expected_msg <- glue::glue("test.csv @ {get_full_path}")
+        mockery::expect_called(log_mock, 1)
+        mockery::expect_args(log_mock, 1, expected_msg)
+      }
+    )
 
-      # Verify log_read was called with correct message
-      expected_msg <- glue::glue("test.csv @ {get_full_path}")
-      mockery::expect_called(log_mock, 1)
-      mockery::expect_args(log_mock, 1, expected_msg)
-    }
-  )
+    test_that(
+      "log_write_connector.ConnectorDatabricksVolume logs correct message",
+      {
+        # Create mock for whirl::log_write
+        log_mock <- mockery::mock()
+        mockery::stub(
+          log_write_connector.ConnectorDatabricksVolume,
+          "whirl::log_write",
+          log_mock
+        )
 
-  test_that(
-    "log_write_connector.ConnectorDatabricksVolume logs correct message",
-    {
-      # Create mock for whirl::log_write
-      log_mock <- mockery::mock()
-      mockery::stub(
-        log_write_connector.ConnectorDatabricksVolume,
-        "whirl::log_write",
-        log_mock
-      )
+        # Test the function
+        log_write_connector.ConnectorDatabricksVolume(
+          db_vol_connector,
+          "test.csv"
+        )
 
-      # Test the function
-      log_write_connector.ConnectorDatabricksVolume(
-        db_vol_connector,
-        "test.csv"
-      )
+        # Verify log_write was called with correct message
+        expected_msg <- glue::glue("test.csv @ {get_full_path}")
+        mockery::expect_called(log_mock, 1)
+        mockery::expect_args(log_mock, 1, expected_msg)
+      }
+    )
 
-      # Verify log_write was called with correct message
-      expected_msg <- glue::glue("test.csv @ {get_full_path}")
-      mockery::expect_called(log_mock, 1)
-      mockery::expect_args(log_mock, 1, expected_msg)
-    }
-  )
+    test_that(
+      "log_remove_connector.ConnectorDatabricksVolume logs correct message",
+      {
+        # Create mock for whirl::log_delete
+        log_mock <- mockery::mock()
+        mockery::stub(
+          log_remove_connector.ConnectorDatabricksVolume,
+          "whirl::log_delete",
+          log_mock
+        )
 
-  test_that(
-    "log_remove_connector.ConnectorDatabricksVolume logs correct message",
-    {
-      # Create mock for whirl::log_delete
-      log_mock <- mockery::mock()
-      mockery::stub(
-        log_remove_connector.ConnectorDatabricksVolume,
-        "whirl::log_delete",
-        log_mock
-      )
+        # Test the function
+        log_remove_connector.ConnectorDatabricksVolume(
+          db_vol_connector,
+          "test.csv"
+        )
 
-      # Test the function
-      log_remove_connector.ConnectorDatabricksVolume(
-        db_vol_connector,
-        "test.csv"
-      )
-
-      # Verify log_delete was called with correct message
-      expected_msg <- glue::glue("test.csv @ {get_full_path}")
-      mockery::expect_called(log_mock, 1)
-      mockery::expect_args(log_mock, 1, expected_msg)
-    }
-  )
-
-
-
+        # Verify log_delete was called with correct message
+        expected_msg <- glue::glue("test.csv @ {get_full_path}")
+        mockery::expect_called(log_mock, 1)
+        mockery::expect_args(log_mock, 1, expected_msg)
+      }
+    )
+  }
 }
 
