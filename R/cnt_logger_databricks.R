60b85e44d0ee8de9e8a441a80661929aff015714
Vladimir Obucina <vlob@novonordisk.com>
2025-04-02 13:46:33 +0200

    Fix Megalinter error for COPY/PASTE.

--- R/cnt_logger_databricks.R
+++ R/cnt_logger_databricks.R
@@ -9,31 +9,11 @@
 #'
 #' @export
 log_read_connector.ConnectorDatabricksTable <- function(
-    connector_object,
-    name,
-    ...) {
-  rlang::check_installed("whirl")
-
-  msg <- paste0(
-    name,
-    " @ ",
-    "dbname: ",
-    connector_object$conn@info$dbname,
-    ", dbms.name: ",
-    connector_object$conn@info$dbms.name,
-    ", db.version: ",
-    connector_object$conn@info$db.version,
-    ", drivername: ",
-    connector_object$conn@info$drivername,
-    ", driver.version: ",
-    connector_object$conn@info$driver.version,
-    ", odbcdriver.version: ",
-    connector_object$conn@info$odbcdriver.version,
-    ", catalog:",
-    connector_object$catalog,
-    ", schema:",
-    connector_object$schema
-  )
+  connector_object,
+  name,
+  ...
+) {
+  msg <- log_connector_message(connector_object, name, ...)
   whirl::log_read(msg)
 }
 
@@ -48,31 +28,11 @@ log_read_connector.ConnectorDatabricksTable <- function(
 #'
 #' @export
 log_write_connector.ConnectorDatabricksTable <- function(
-    connector_object,
-    name,
-    ...) {
-  rlang::check_installed("whirl")
-
-  msg <- paste0(
-    name,
-    " @ ",
-    "dbname: ",
-    connector_object$conn@info$dbname,
-    ", dbms.name: ",
-    connector_object$conn@info$dbms.name,
-    ", db.version: ",
-    connector_object$conn@info$db.version,
-    ", drivername: ",
-    connector_object$conn@info$drivername,
-    ", driver.version: ",
-    connector_object$conn@info$driver.version,
-    ", odbcdriver.version: ",
-    connector_object$conn@info$odbcdriver.version,
-    ", catalog:",
-    connector_object$catalog,
-    ", schema:",
-    connector_object$schema
-  )
+  connector_object,
+  name,
+  ...
+) {
+  msg <- log_connector_message(connector_object, name, ...)
   whirl::log_write(msg)
 }
 
@@ -87,31 +47,11 @@ log_write_connector.ConnectorDatabricksTable <- function(
 #'
 #' @export
 log_remove_connector.ConnectorDatabricksTable <- function(
-    connector_object,
-    name,
-    ...) {
-  rlang::check_installed("whirl")
-
-  msg <- paste0(
-    name,
-    " @ ",
-    "dbname: ",
-    connector_object$conn@info$dbname,
-    ", dbms.name: ",
-    connector_object$conn@info$dbms.name,
-    ", db.version: ",
-    connector_object$conn@info$db.version,
-    ", drivername: ",
-    connector_object$conn@info$drivername,
-    ", driver.version: ",
-    connector_object$conn@info$driver.version,
-    ", odbcdriver.version: ",
-    connector_object$conn@info$odbcdriver.version,
-    ", catalog:",
-    connector_object$catalog,
-    ", schema:",
-    connector_object$schema
-  )
+  connector_object,
+  name,
+  ...
+) {
+  msg <- log_connector_message(connector_object, name, ...)
   whirl::log_delete(msg)
 }
 
@@ -126,9 +66,10 @@ log_remove_connector.ConnectorDatabricksTable <- function(
 #'
 #' @export
 log_read_connector.ConnectorDatabricksVolume <- function(
-    connector_object,
-    name,
-    ...) {
+  connector_object,
+  name,
+  ...
+) {
   rlang::check_installed("whirl")
   msg <- paste0(name, " @ ", connector_object$full_path)
   whirl::log_read(msg)
@@ -145,9 +86,10 @@ log_read_connector.ConnectorDatabricksVolume <- function(
 #'
 #' @export
 log_write_connector.ConnectorDatabricksVolume <- function(
-    connector_object,
-    name,
-    ...) {
+  connector_object,
+  name,
+  ...
+) {
   rlang::check_installed("whirl")
   msg <- paste0(name, " @ ", connector_object$full_path)
   whirl::log_write(msg)
@@ -164,10 +106,42 @@ log_write_connector.ConnectorDatabricksVolume <- function(
 #'
 #' @export
 log_remove_connector.ConnectorDatabricksVolume <- function(
-    connector_object,
-    name,
-    ...) {
+  connector_object,
+  name,
+  ...
+) {
   rlang::check_installed("whirl")
   msg <- paste0(name, " @ ", connector_object$full_path)
   whirl::log_delete(msg)
+}
+
+
+#' Log connector message
+#'
+#' Generate connector object log message with all the informations
+#' about the object.
+#' @noRd
+#' @keywords internal
+log_connector_message <- function(connector_object, name) {
+  rlang::check_installed("whirl")
+  msg <- paste0(
+    name,
+    " @ ",
+    "dbname: ",
+    connector_object$conn@info$dbname,
+    ", dbms.name: ",
+    connector_object$conn@info$dbms.name,
+    ", db.version: ",
+    connector_object$conn@info$db.version,
+    ", drivername: ",
+    connector_object$conn@info$drivername,
+    ", driver.version: ",
+    connector_object$conn@info$driver.version,
+    ", odbcdriver.version: ",
+    connector_object$conn@info$odbcdriver.version,
+    ", catalog:",
+    connector_object$catalog,
+    ", schema:",
+    connector_object$schema
+  )
 }
